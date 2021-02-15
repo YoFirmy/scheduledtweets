9e366203441623c72110e5bd715b5e163bd21d00
Matt Firmston <mattfirmston@gmail.com>
2021-02-15 16:45:02 +0000

    added sign up error alerts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password 
   
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-  validates :password, presence: true, format: { with: /\A\s{6,}\z/ }
+  validates :password, presence: true, format: { with: /\A\s{5,}\z/ }
 end
