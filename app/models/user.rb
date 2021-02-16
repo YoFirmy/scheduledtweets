3ad49c0d4e42a58ef975df76b89a10f12f7a0c63
Matt Firmston <mattfirmston@gmail.com>
2021-02-16 13:27:58 +0000

    enabled users sessions and fixed password regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password 
   
   validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-  validates :password, presence: true, format: { with: /\A\s{5,}\z/ }
+  validates :password, presence: true, format: { with: /\A\S{5,}\z/ }
 end
